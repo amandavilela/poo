836ac17f02b461472c4eff016cec426f068d4605
Amanda Vilela <amandavilelaalmeida@gmail.com>
2015-09-27 12:47:34 -0300

    Includes subclass PessoaJuridica

--- prjPessoaFisicaPessoaJuridica/src/fatec/poo/aula3/PessoaJuridica.java
+++ prjPessoaFisicaPessoaJuridica/src/fatec/poo/aula3/PessoaJuridica.java
@@ -5,10 +5,26 @@ public class PessoaJuridica extends Pessoa{
     private String cgc;
     private double taxaIncentivo;
     
-    public PessoaJuridica(String nome, int anoInscricao, String cgc, double taxaIncentivo){
+    public PessoaJuridica(String nome, int anoInscricao, String cgc){
         super(nome, anoInscricao);
         this.cgc = cgc;
+    }
+
+    public String getCgc() {
+        return cgc;
+    }
+
+    public double getTaxaIncentivo() {
+        return taxaIncentivo;
+    }
+
+    public void setTaxaIncentivo(double taxaIncentivo) {
         this.taxaIncentivo = taxaIncentivo;
+    }
+    
+    @Override
+    public double calcBonus(int anoAtual){
+        return ((taxaIncentivo*super.totalCompras)*(anoAtual-super.anoInscricao));
     }
     
 }
