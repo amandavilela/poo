cfa4a2c5227ab7eee520b5a5ee0ec03d8e906012
bernardes_felipe <bernardes.flp@gmail.com>
2015-09-27 12:18:13 -0300

    Includes abstract class Pessoa

--- prjPessoaFisicaPessoaJuridica/src/fatec/poo/aula3/Pessoa.java
+++ prjPessoaFisicaPessoaJuridica/src/fatec/poo/aula3/Pessoa.java
@@ -1,29 +1,32 @@
 package fatec.poo.aula3;
 
-public class Pessoa {
+public abstract class Pessoa {
     private String nome;
-    private String dataNasc;
-    private String email;
+    private int anoInscricao;
+    private double totalCompras;
     
-    public Pessoa(String n, String dt){
-        nome = n;
-        dataNasc = dt;
+    public Pessoa(String nome, int anoInscricao){
+        this.nome = nome;
+        this.anoInscricao = anoInscricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoInscricao() {
+        return anoInscricao;
+    }
+
+    public double getTotalCompras() {
+        return totalCompras;
     }
     
-    public void setEmail(String em){
-        email = em;
-    }
+    abstract public double calcBonus();
     
-    public String getNome(){
-        return(nome);
+    public void addCompras(double vlrCompra) {
+        this.totalCompras += vlrCompra;
     }
-    
-    public String getDataNasc(){
-        return(dataNasc);
-    }
-    
-    public String getEmail(){
-        return(email);
-    }
+
 }    
  
