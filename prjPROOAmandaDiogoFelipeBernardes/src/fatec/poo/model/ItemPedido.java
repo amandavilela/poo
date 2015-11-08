be7dff4078b8c0cd11f70fb12080569c11707b49
bernardes_felipe <bernardes.flp@gmail.com>
2015-11-08 10:50:21 -0200

    Creates ItemPedido class

---
+++ prjPROOAmandaDiogoFelipeBernardes/src/fatec/poo/model/ItemPedido.java
@@ -0,0 +1,23 @@
+package fatec.poo.model;
+
+public class ItemPedido {
+    private int numeroItem;
+    private int qtdeVendida;
+    
+    public ItemPedido(int numeroItem, int qtdeVendida) {
+        this.numeroItem = numeroItem;
+        this.qtdeVendida = qtdeVendida;
+    }
+
+    public int getNumeroItem() {
+        return numeroItem;
+    }
+
+    public int getQtdeVendida() {
+        return qtdeVendida;
+    }
+
+    public void setQtdeVendida(int qtdeVendida) {
+        this.qtdeVendida = qtdeVendida;
+    }
+}
